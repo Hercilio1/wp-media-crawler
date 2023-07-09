400f7b85828c804970004612365b44ef9e20a917
Hercilio M. Ortiz <herciliomartins@gmail.com>
2023-07-09 00:48:08 -0300

    Testing: Refactoring the bootstrap structure of unit tests

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -11,12 +11,12 @@
  * File that will be run by PHPUnit before testings
  */
 
-define( 'WP_MEDIA_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . '/Fixtures' );
+define( 'WP_MEDIA_PLUGIN_ROOT', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
+define( 'WP_MEDIA_PLUGIN_TESTS_ROOT', dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'Unit' . DIRECTORY_SEPARATOR );
+define( 'WP_MEDIA_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'Fixtures' );
 
-if ( file_exists( dirname( dirname( __DIR__ ) ) . '/vendor/autoload.php' ) ) {
-	require_once dirname( dirname( __DIR__ ) ) . '/vendor/autoload.php';
-}
-
+// For some reason some of the classes inside the \\Tests\\Unit namespace are not being loaded by the wp
+// media bootstrap.php. So we need to load them manually.
 $loader = new \Composer\Autoload\ClassLoader();
 $loader->addPsr4( 'WP_Media\\Crawler\\Tests\\', dirname( __DIR__ ), true );
 $loader->register();
